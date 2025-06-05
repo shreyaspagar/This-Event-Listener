af82094868670f0136e78ae49ed48886599a2a47
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-06-05 21:32:27 +0530

    Add files via upload

---
+++ app114.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector("button");
+
+
+
+
+
+
+// btn.addEventListener("click", function(event) {
+//     console.log(event);
+//     console.log("button clicked");
+// });    
+
+btn.addEventListener("dblclick", function(event) {
+    console.log(event);
+    console.log("button clicked");
+});  
